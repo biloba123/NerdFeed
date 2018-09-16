c5c14072d8a7014c4f1c35f8d5a38f91908fc55f
吕晴阳 <lvqingyang@bytedance.com>
2018-09-16 14:47:33 +0800

    Initial Commit

---
+++ NerdFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NerdFeed
+//
+//  Created by 吕晴阳 on 2018/9/16.
+//  Copyright © 2018年 Lv Qingyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NerdFeed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NerdFeed
+//
+//  Created by 吕晴阳 on 2018/9/16.
+//  Copyright © 2018年 Lv Qingyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
